91d3b1bc69fd7c373ebbd9cd5b7f0db953ed469f
anurag23s <113811615+anurag23s@users.noreply.github.com>
2023-08-18 16:36:14 +0530

    app.js with stack

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer} from '@react-navigation/native' ;
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen 
+        name="SelectScreen"
+        component={Chooseuser}
+        options={{ headerShown: false,}}
+      />
+      <Stack.Screen 
+        name="Driver"
+        component={DriverLogin}
+        options={{ headerShown: false,}}
+      /> 
+      <Stack.Screen 
+        name="Student"
+        component={Student_route_select}
+        options={{ headerShown: false,}}
+      /> 
+
+      </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
