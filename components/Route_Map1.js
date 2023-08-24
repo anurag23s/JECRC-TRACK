3de91e1c010885907c4e2e51ddbeec26bde855b2
Ansh Goyal <anshgoyal0604@gmail.com.com>
2023-08-24 11:37:58 +0530

    menu

--- components/Route_Map1.js
+++ components/Route_Map1.js
@@ -1,28 +1,35 @@
 import React, { Component, useState, useEffect, useRef} from "react";
-import { StyleSheet, Text, View ,TouchableOpacity, FlatList, SafeAreaView, ImageBackground, Button } from "react-native";
+import { StyleSheet, Text, View ,TouchableOpacity, FlatList, SafeAreaView, ImageBackground, Button ,Animated,Dimensions} from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import MapViewDirections from "react-native-maps-directions";
 import { GOOGLE_MAPS_APIKEY } from "@env";
 import { db1 } from '../src/screens/firebase';
 import { onValue, off, ref } from 'firebase/database';
-import MIcon from 'react-native-vector-icons/MaterialCommunityIcons';
-import Icon from 'react-native-vector-icons/FontAwesome';
 import tw from 'tailwind-react-native-classnames';
 //import * as Location from 'expo-location';
-
-
-
-
-
-
+import MIco from 'react-native-vector-icons/Foundation';
+import MIconss from 'react-native-vector-icons/FontAwesome5';
+import MIcons from 'react-native-vector-icons/MaterialIcons';
+import AntDesign from "react-native-vector-icons/Feather";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import {LinearGradient} from 'expo-linear-gradient';
 import { LogBox } from 'react-native';
 LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
 LogBox.ignoreAllLogs();//Ignore all log notifications
+import { useNavigation } from '@react-navigation/native';
+
+
+
+const { width: screenWidth } = Dimensions.get('window');
 
 export default function Route_Map1() {
+
+  const navigation = useNavigation();
   const [latitude, setLatitude] = useState(0);
   const [longitude, setLongitude] = useState(0);
+
 
   useEffect(() => {
     const db = db1;
@@ -55,6 +62,34 @@ export default function Route_Map1() {
       off(databaseRef);
     };
   }, []);
+
+
+  const [menuVisible, setMenuVisible] = useState(false);
+  const menuAnimation = useRef(new Animated.Value(0)).current;
+
+  const toggleMenu = () => {
+    const toValue = menuVisible ? 0 : 1;
+    Animated.timing(menuAnimation, {
+      toValue,
+      duration: 300,
+      useNativeDriver: false,
+    }).start();
+    setMenuVisible(!menuVisible);
+  };
+
+  const closeMenu = () => {
+    Animated.timing(menuAnimation, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: false,
+    }).start();
+    setMenuVisible(false);
+  };
+
+  const translateX = menuAnimation.interpolate({
+    inputRange: [0, 1],
+    outputRange: [-screenWidth, 0],
+  });
 
 
    const name = [
@@ -205,10 +240,19 @@ export default function Route_Map1() {
   const {loca,locb,locc,locd,loce,locf,locg,loch,loci,locj,lockk,locl,locm,locn,loco,locp,locq,locr,locs} = state
   
     return (
-        <SafeAreaView>
-        <View style={tw`flex:1`}>
-          <View  style={tw`  h-1/2`}>
-     
+      <SafeAreaView>
+    <View style={tw`flex:1 bg-white `}>
+  
+  
+      <View  style={tw`   h-4/5`}>
+        <Text style={tw`left-28  font-bold top-10 text-xl`}>  NIRWARU- JECRC         </Text>
+      <TouchableOpacity   
+                    style={tw` p-2  `}
+                     onPress={toggleMenu}   >
+        <AntDesign style={tw`x-2 y-6`}
+          name={"menu"} size={35} /> 
+       </TouchableOpacity>
+       
      <MapView
     style={{ flex: 1 }}
     initialRegion={{
@@ -406,67 +450,125 @@ export default function Route_Map1() {
 
 
 
-  <View  style={tw`  h-1/2 bg-white `}>
-<Text  style={tw` text-2xl top-2 text-center font-bold  ` }>
-  Route Details
-</Text>
-<View style={tw `border-t border-black mt-3`}></View>
-<View  style={tw`aligned-center  rounded-half  top-2 justify-evenly justify-center h-3/4 bottom-32`}>
-<View   style={{height: '100%', width: '100%'}}>
-    <LinearGradient 
-     colors={['#f9f8dd', '#302a75']} 
-      style={{height: '100%'}}> 
-  <View>
-  <FlatList 
+  <Animated.View
+        style={{
+          position: 'absolute',
+          left: -10,
+          top: 10,
+          width: '70%',
+          height: "1200%",
+          backgroundColor: 'white',
+          transform: [{translateX}],
+          zIndex: 1,
+          
+          borderBottomRightRadius: 40,
+        }}
+      >
 
-    data={name}
-    keyExtractor={(inx) => inx.toString()}
-    renderItem={({ item ,inx}) => (
-    <View style={tw`text-center justify-center `}>
+        <Text style={tw`text-4xl font-bold top-4 p-2 text-center`}> Menu </Text>
+      
+        <View style={tw `border-t  border-2 border-black mt-8 `}></View>
 
-           {inx > 0 && <Text style={tw` mx-48 `}>•</Text>} 
-           {inx > 0 && <Text style={tw` mx-48 `}>•</Text>} 
-       <Text style={tw` mx-28 text-center  `}>{item.text}</Text>     
-       <MIcon 
-       style={tw` mx-28 bottom-5`}
-       name="bus-stop" size={20} color="red" /> 
+         <TouchableOpacity style={tw` m-3  top-4  rounded-full bg-blue-400  p-2`} onPress={ () => navigation.navigate("About")}>
+         <MIconss style={tw` p-3 absolute  `}
+          name="route" size={15} color="black" />
+           <Text style={tw`  text-left  text-lg  left-4`}>   Bus Route</Text>
+         </TouchableOpacity>
 
-    </View> 
+
+         <TouchableOpacity style={tw` m-3 top-2 p-2 rounded-full bg-blue-400   `} onPress={ () => navigation.navigate("About")}>
+          <Ionicons style={tw` p-3   absolute `}
+          
+          name="person-outline" size={22} color="black" />
+           <Text style={tw` text-lg text-left  left-4`}>    Driver Details </Text>
+         </TouchableOpacity>
+
+         <TouchableOpacity style={tw` m-3 x-2  top-1 rounded-full bg-blue-400  p-2`} onPress={ () => navigation.navigate("About")}>
+          
          
-  )}
-  
-  />
-  </View>
-   </LinearGradient>
-</View>
-</View>
+         <Ionicons style={tw` p-3   absolute `}
+         name="chatbox-ellipses-outline" size={22} color="black" />
+
+           <Text style={tw` text-lg text-left  left-4`}>    Emergency Contact</Text>
+         </TouchableOpacity>
+
+
+
+         <TouchableOpacity style={tw` m-3 x-2   rounded-full bg-blue-400  p-2`} onPress={ () => navigation.navigate("About")}>
+        
+         <Ionicons style={tw` p-3  absolute `}
+         name="information-circle-outline" size={25} color="black" />
+
+           <Text style={tw` text-lg text-left  left-4`}>    About us</Text>
+         </TouchableOpacity>
+         <TouchableOpacity style={tw` m-3 x-2   rounded-full bg-blue-400  p-2`} onPress={ () => navigation.navigate("About")}>
+        
+         <MIcons style={tw` p-3  absolute `}
+          name="report" size={22}  color="black" />
+
+          <Text style={tw` text-lg text-left  left-4`}>    ReportIssue</Text>
+        </TouchableOpacity>
+
+         <TouchableOpacity style={tw` m-3 x-2   rounded-full bg-blue-400  p-2`} onPress={ () => navigation.navigate("About")}>
+        
+        < MIco style={tw` p-3  absolute `}
+        name="clipboard-pencil" size={25} color="black" />
+
+          <Text style={tw` text-lg text-left  left-4`}>   Terms and conditions </Text>
+        </TouchableOpacity>
+
+
+
+         
+         
+         </Animated.View> 
+         <View 
     
-  <View 
-    
-    style={tw` flex:1  bg-gray-300 h-1/4 bottom-11`}>
-   <Icon style={tw`  top-8 left-2 absolute  `}
-       name="user-circle" size={50} color="white" />
+    style={tw` flex:1 bg-gray-200  h-1/5 `}>
+
+   <Icon style={tw` top-16 left-3 absolute  `}
+       name="user-circle" size={60} color="white" />
  
-  <Text   style={tw` text-xl  text-center  font-bold ` }>
+  <Text   style={tw` text-2xl  text-center  font-bold ` }>
   
     Driver Details
     
   </Text>
- <Text  style={tw`  left-16` }>
+  
+  <View style={tw `border-t border-black mt-2`}></View>
+  
+  <View style={tw`top-3 left-4`}>
+ <Text  style={tw`   left-16  text-lg` }>
   Name: Ram ji
  </Text>
- <Text  style={tw`  left-16` }>
+ <Text  style={tw`  left-16 text-lg ` }>
   Contact no: 48942454
  </Text>
- <Text  style={tw`  left-16` }>
+ <Text  style={tw`  left-16 text-lg` }>
   Bus No: RJ14GC7643
+ </Text>
+
+
+ <Text  style={tw`  left-16` }>
+  
+ </Text>
+ <Text  style={tw`  left-16` }>
+  
  </Text>
  
  
   </View>
-
-</View>
   </View>
+</View>
+  
   </SafeAreaView>
     );
   };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
